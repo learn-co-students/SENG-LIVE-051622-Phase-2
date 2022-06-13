3ee954149ed935ccdfbd4d4b4f937f710ce82978
Aysan Isayo <aisayo@icloud.com>
2022-06-13 15:44:52 -0400

    solution code

--- 05_side_effects_and_data_fetching/project_showcase/src/App.js
+++ 05_side_effects_and_data_fetching/project_showcase/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
 import ProjectForm from "./components/ProjectForm";
 import ProjectList from "./components/ProjectList";
@@ -6,11 +6,27 @@ import ProjectList from "./components/ProjectList";
 const App = () => {
   const [projects, setProjects] = useState([]);
   const [isDarkMode, setIsDarkMode] = useState(true);
+  const [count, setCount] = useState(0);
 
-  const handleClick = () => {
+  console.log("component rendering");
+
+  useEffect(() => {
     fetch("http://localhost:4000/projects")
-      .then((res) => res.json())
+      .then((resp) => resp.json())
       .then((projects) => setProjects(projects));
+  }, []);
+
+  useEffect(() => {
+    console.log("side effect happening");
+
+    return () => {
+      console.log("clean up phase");
+    };
+  }, [count]);
+
+  const onAddProject = (newProject) => {
+
+    setProjects([...projects, newProject]);
   };
 
   const onToggleDarkMode = () => setIsDarkMode(!isDarkMode);
@@ -18,9 +34,9 @@ const App = () => {
   return (
     <div className={isDarkMode ? "App" : "App light"}>
       <Header isDarkMode={isDarkMode} onToggleDarkMode={onToggleDarkMode} />
-      <ProjectForm />
-      <button onClick={handleClick}>Load Projects</button>
+      <ProjectForm onAddProject={onAddProject}/>
       <ProjectList projects={projects} />
+      <button onClick={() => setCount(count + 1)}>{count}</button>
     </div>
   );
 };
--- 05_side_effects_and_data_fetching/project_showcase/src/components/ProjectForm.js
+++ 05_side_effects_and_data_fetching/project_showcase/src/components/ProjectForm.js
@@ -1,19 +1,68 @@
-import React from "react";
+import { useState } from "react";
 
-const ProjectForm = () => {
+const ProjectForm = ({ onAddProject }) => {
+  const initialState = {
+    name: "",
+    about: "",
+    phase: "",
+    link: "",
+    image: "",
+  };
+
+  const [formData, setFormData] = useState(initialState);
+
+  console.log(formData);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((previousFormData) => ({ ...previousFormData, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    fetch("http://localhost:4000/projects", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accepts": "application/json"
+      },
+      body: JSON.stringify(formData)
+    })
+    .then(resp => resp.json())
+    .then(project => onAddProject(project))
+    setFormData(initialState);
+  };
+
   return (
     <section>
-      <form className="form" autoComplete="off">
+      <form className="form" autoComplete="off" onSubmit={handleSubmit}>
         <h3>Add New Project</h3>
 
         <label htmlFor="name">Name</label>
-        <input type="text" id="name" name="name" />
+        <input
+          type="text"
+          id="name"
+          name="name"
+          onChange={handleChange}
+          value={formData.name}
+        />
 
         <label htmlFor="about">About</label>
-        <textarea id="about" name="about" />
+        <textarea
+          id="about"
+          name="about"
+          onChange={handleChange}
+          value={formData.about}
+        />
 
         <label htmlFor="phase">Phase</label>
-        <select name="phase" id="phase">
+        <select
+          name="phase"
+          id="phase"
+          onChange={handleChange}
+          value={formData.phase}
+        >
           <option>Select One</option>
           <option value="1">Phase 1</option>
           <option value="2">Phase 2</option>
@@ -23,10 +72,22 @@ const ProjectForm = () => {
         </select>
 
         <label htmlFor="link">Project Homepage</label>
-        <input type="text" id="link" name="link" />
+        <input
+          type="text"
+          id="link"
+          name="link"
+          onChange={handleChange}
+          value={formData.link}
+        />
 
         <label htmlFor="image">Screenshot</label>
-        <input type="text" id="image" name="image" />
+        <input
+          type="text"
+          id="image"
+          name="image"
+          onChange={handleChange}
+          value={formData.image}
+        />
 
         <button type="submit">Add Project</button>
       </form>
